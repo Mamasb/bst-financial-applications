854f95556e678376f3d5312e6e3ec4c3ddcd6ca4
mamasb <pkpipimwangi@gmail.com>
2024-06-21 13:42:11 +0300

    Header file containing BST structure definitions and function prototypes.

---
+++ bst.h
@@ -0,0 +1,17 @@
+#ifndef BST_H
+#define BST_H
+
+typedef struct bst_node {
+    int data;
+    struct bst_node *left;
+    struct bst_node *right;
+} bst_t;
+
+bst_t *create_new_node(int data);
+void inorder(bst_t *root);
+bst_t *insert(bst_t *root, int data);
+bst_t *findmin(bst_t *root);
+int search(bst_t *root, int data);
+bst_t *delete_node(bst_t *root, int data);
+
+#endif /* BST_H */
