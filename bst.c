2495d9c6f787133a0d0c1e7e3d7e9a30c1d405ec
mamasb <pkpipimwangi@gmail.com>
2024-06-21 13:54:08 +0300

    Source file containing BST function implementations.

--- bst.c
+++ bst.c
@@ -1,5 +1,6 @@
-#include "bst.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "bst.h"
 
 /* Create New Nodes */
 bst_t *create_new_node(int data)
@@ -7,7 +8,6 @@ bst_t *create_new_node(int data)
     bst_t *new_node = malloc(sizeof(bst_t));
     if (new_node == NULL)
         return NULL;
-    
     new_node->data = data;
     new_node->left = NULL;
     new_node->right = NULL;
@@ -19,7 +19,6 @@ void inorder(bst_t *root)
 {
     if (root == NULL)
         return;
-    
     inorder(root->left);
     printf("%d ", root->data);
     inorder(root->right);
@@ -29,66 +28,71 @@ void inorder(bst_t *root)
 bst_t *insert(bst_t *root, int data)
 {
     if (root == NULL)
-        return create_new_node(data);
-
-    if (data <= root->data)
+        root = create_new_node(data);
+    else if(data <= root->data)
         root->left = insert(root->left, data);
     else
         root->right = insert(root->right, data);
-    
     return root;
 }
 
-/* Find the minimum value in the tree */
+/* Find a minimum value of a tree */
 bst_t *findmin(bst_t *root)
 {
-    while (root->left != NULL)
+    while(root->left != NULL)
         root = root->left;
-    
     return root;
 }
 
-/* Search for an element in BST */
+/* Search an element from BST */
 int search(bst_t *root, int data)
 {
     if (root == NULL)
         return 0;
-    else if (root->data == data)
+    else if(root->data == data)
         return 1;
-    else if (data < root->data)
-        return search(root->left, data);
+    else if(data <= root->data)
+        return search(root->left,data);
     else
-        return search(root->right, data);
+        return search(root->right,data);
 }
 
-/* Delete a node from BST */
+/* Delete a node from a tree */
 bst_t *delete_node(bst_t *root, int data)
 {
     if (root == NULL)
         return root;
-
-    if (data < root->data)
+    else if(data < root->data)
         root->left = delete_node(root->left, data);
-    else if (data > root->data)
+    else if(data > root->data)
         root->right = delete_node(root->right, data);
     else
     {
-        if (root->left == NULL)
+        /* Delete a node with no child */
+        if(root->left == NULL && root->right == NULL)
         {
-            bst_t *temp = root->right;
             free(root);
-            return temp;
+            root = NULL;
         }
-        else if (root->right == NULL)
+        /* Delete a node with one child */
+        else if(root->left == NULL)
         {
-            bst_t *temp = root->left;
-            free(root);
-            return temp;
+            bst_t *temp = root;
+            root = root->right;
+            free(temp);
         }
-
-        bst_t *temp = findmin(root->right);
-        root->data = temp->data;
-        root->right = delete_node(root->right, temp->data);
+        else if(root->right == NULL)
+        {
+            bst_t *temp = root;
+            root = root->left;
+            free(temp);
+        }
+        /* Delete a node with two children */
+        else {
+            bst_t *temp = findmin(root->right);
+            root->data = temp->data;
+            root->right = delete_node(root->right,temp->data);
+        }
     }
     return root;
 }
